d8cc93bbd837f3bb7b27cec04a6daacf66f34b73
Nathan Pannell <n8tewolf@gmail.com>
2024-08-18 18:53:46 -0700

    refactor: renamed client

---
+++ client/src/components/DeepAnalytics.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DeepAnalytics = () => {
+  return <div>DeepAnalytics</div>;
+};
+
+export default DeepAnalytics;
---
+++ client/src/components/TrackBreakdown.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TrackBreakdown = () => {
+  return <div>TrackBreakdown</div>;
+};
+
+export default TrackBreakdown;
